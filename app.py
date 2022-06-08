424e286fc8a97fd71f0a9d18e22c6041a7d94830
HarrisonShort <work.harrisonshort@gmail.com>
2022-06-08 15:17:39 +1000

    ADD updated app.py from tutorial

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+import pulumi.automation as auto
 
 
-if __name__ == '__main__':
+def ensure_plugins():
+    ws = auto.LocalWorkspace()
+    ws.install_plugin("aws", "v4.0.0")
 
-    app.run()
+
+def create_app():
+    ensure_plugins()
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="secret",
+        PROJECT_NAME="heroku-clone",
+        PULUMI_ORG=os.environ.get("PULUMI_ORG"),
+    )
+
+    @app.route("/", methods=["GET"])
+    def index():
+        return render_template("index.html")
+
+    import sites
+
+    app.register_blueprint(sites.bp)
+
+    import virtual_machines
+
+    app.register_blueprint(virtual_machines.bp)
+
+    return app
